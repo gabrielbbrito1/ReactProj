53ed9f691ba7f84362b558fa2a8b0431871367b5
Gabriel <gabrielbbrito_1@hotmail.com>
2023-05-02 10:35:30 -0300

    Adicionando testes em jest para o button

---
+++ src/components/Button/Button.spec.jsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event";
+
+import { Button } from '.';
+
+describe('<Button />', () =>{
+    it('Should render the button with the text "Load more"', () => {
+        render(<Button text ="Load more"/>)
+
+        expect.assertions(1)
+
+        const button = screen.getByRole('button', {name: /load more/i})
+        expect(button).toBeInTheDocument()
+    })
+    it('Should call function on button click', () => {
+        const fn = jest.fn();
+        render(<Button text ="Load more" onClick={fn}/>)
+
+        const button = screen.getByRole('button', {name: /load more/i})
+        userEvent.click(button)
+
+        expect(fn).toHaveBeenCalledTimes(1)
+
+    })
+    it('Should be enabled when disabled is false', () => {
+        render(<Button text ="Load more" disabled={false}/>)
+        const button = screen.getByRole('button', {name: /load more/i})
+        expect(button).toBeEnabled()
+
+    })
+})
